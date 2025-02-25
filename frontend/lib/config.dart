945db62ab500ffea0ce18c190a8861e892ee457d
Sam Trafton <samatrafton@gmail.com>
2025-02-25 13:44:43 +0200

    prod endpoint

--- frontend/lib/config.dart
+++ frontend/lib/config.dart
@@ -1,5 +1,5 @@
 ﻿class ApiConfig {
-  static const String baseUrl = 'https://localhost:7217';
+  static const String baseUrl = 'backend-ptp-test-cncxemfebmfpagfk.israelcentral-01.azurewebsites.net';
   static const String preTaskPlanEndpoint = '$baseUrl/api/PreTaskPlan';
   static const String signAndSubmitEndpoint = '$baseUrl/api/FormSubmission';
   
